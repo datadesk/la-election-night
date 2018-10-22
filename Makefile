c201f4371e2c6f6e2ddd80c6481dcebdd6dc89a6
palewire <ben.welsh@gmail.com>
2018-10-22 16:17:36 -0700

    Got setup.py right

--- Makefile
+++ Makefile
@@ -1,4 +1,10 @@
+.PHONY: test ship
+
 test:
 	flake8 la_election_night
 	coverage run tests.py
 	coverage report -m
+
+ship:
+	python setup.py sdist bdist_wheel
+	twine upload dist/* --skip-existing
---
+++ setup.py
@@ -0,0 +1,28 @@
+import os
+from setuptools import setup
+from distutils.core import Command
+
+
+setup(
+    name='la-election-night',
+    version='0.0.1',
+    description="Parse the results file published on election night by the Los Angeles County Registrar-Recorder/County Clerk",
+    author='Los Angeles Times Data Desk',
+    author_email='datadesk@latimes.com',
+    url='http://www.github.com/datadesk/la-election-night',
+    license="MIT",
+    packages=("la_election_night",),
+    include_package_data=True,
+    zip_safe=False,  # because we're including static files
+    install_requires=(),
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'License :: OSI Approved :: MIT License',
+    ],
+)
